71ee2493fc7747e523e49d6a83e5b834007178e1
Aayush Rana <aayushsenpei@gmail.com>
2026-09-20 16:09:01 +0530

    20-09-26

---
+++ 11-form-handling/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const App = () => {
+  const submitHandler = (elem) => {
+    elem.preventDefault();
+    console.log("Form Submitted");
+  };
+  return (
+    <div>
+      <form
+        onClick={(elem) => {
+          submitHandler(elem);
+        }}
+      >
+        <input type="text" placeholder="Enter your name " />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
