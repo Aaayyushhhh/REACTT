5c11ba5e5f91dbafebbc9e44eb615d24b7fb8ad4
Aayush Rana <aayushsenpei@gmail.com>
2026-09-24 00:45:06 +0530

    24-09-26

--- 18-RRD/src/App.jsx
+++ 18-RRD/src/App.jsx
@@ -1,28 +1,21 @@
 import React from "react";
-import { Routes } from "react-router-dom";
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Navbar from "./Components/Navbar";
+import Product from "./pages/Product";
 
 const App = () => {
   return (
     <div>
-      <div className="nav">
-        <h3>Shreyains</h3>
-        <div>
-          <a href="/">Home</a>
-          <a href="/about">About</a>
-          <a href="/Contact">Contact</a>
-        </div>
-      </div>
-
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/Contact" element={<Contact />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product" element={<Product />} />
       </Routes>
-      <h2>This is Footer</h2>
     </div>
   );
 };
